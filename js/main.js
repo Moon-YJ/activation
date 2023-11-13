4c875684505eb8c5b88fe2d923493b480fe9b4f4
Moon-YJ <tunjeong@gmail.com>
2023-11-13 11:35:16 +0900

    활성화버튼 클릭시 함수 재호출 방지

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ const articles = main.querySelectorAll('section article');
 // 버튼 클릭 이벤트문
 btns.forEach((btn, i) => {
 	btn.addEventListener('click', () => {
+		// 클릭한 버튼에 on 클래스 여부 확인(있으면 return으로 함수 실행 중지)
+		if (btns[i].classList.contains('on')) return;
 		activation(btns, i);
 		activation(articles, i);
 		//[btns, articles].forEach((el) => activation(el, i));
@@ -14,6 +16,7 @@ btns.forEach((btn, i) => {
 // 인수로 그룹 선택자, 순번을 받아서
 // 순번의 요소만 활성화시키는 함수
 const activation = (arr, idx) => {
+	console.log('activation');
 	arr.forEach((el) => el.classList.remove('on'));
 	arr[idx].classList.add('on');
 };
