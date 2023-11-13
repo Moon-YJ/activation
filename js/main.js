e3df07b9ba3b0118b21cc80962c15d8ac7ea25d9
Moon-YJ <tunjeong@gmail.com>
2023-11-13 11:07:53 +0900

    이벤트함수 패키징하여 재활용

--- js/main.js
+++ js/main.js
@@ -2,18 +2,18 @@ const main = document.querySelector('main');
 const btns = main.querySelectorAll('ul li');
 const articles = main.querySelectorAll('section article');
 
-const activation = (arr, idx) => {
-	arr.forEach((el) => {
-		el.classList.remove('on');
-	});
-	arr[idx].classList.add('on');
-};
-
+// 버튼 클릭 이벤트문
 btns.forEach((btn, i) => {
 	btn.addEventListener('click', () => {
-		btns.forEach((btn) => btn.classList.remove('on'));
-		btns[i].classList.add('on');
-		articles.forEach((article) => article.classList.remove('on'));
-		articles[i].classList.add('on');
+		activation(btns, i);
+		activation(articles, i);
+		//[btns, articles].forEach((el) => activation(el, i));
 	});
 });
+
+// 인수로 그룹 선택자, 순번을 받아서
+// 순번의 요소만 활성화시키는 함수
+const activation = (arr, idx) => {
+	arr.forEach((el) => el.classList.remove('on'));
+	arr[idx].classList.add('on');
+};
